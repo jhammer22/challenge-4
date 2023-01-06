885edb6d26708ad28d7b952940e08c108cac09fa
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-01-05 23:25:37 -0600

    js and css pseudo added

--- script.js
+++ script.js
@@ -6,3 +6,31 @@
 // next-button
 // finitial
 // secinitial
+
+
+
+
+// click event when start button is pressed do below
+  // click up or down?
+
+// if start button is pressed display question 
+  // if not display null
+  // if start button is pressed start timer
+  // return hidden question
+
+// questions
+  // event listener for answer
+  // way to check if answer is true or false
+  // store answer
+  // if true reveal next question
+  // if false subtract 15sec and move to next question
+  // if timer reaches 0 stop game display score and do bellow
+
+// once all questions have been answered and score is logged
+  // display score
+  // reveal initials form
+  // store score  and initial in local to be called by     leader board     
+  // display score to user in leader board
+  // display initials next to score in leader board
+  
+
