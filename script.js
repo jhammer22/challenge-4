07c7034669952c6a774cf894bd12c2c75dc8d29c
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-01-08 22:42:38 -0600

    all buttons functional need to add remaining questions and timer

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ let shuffledQuestions, currentQuestionIndex;
 
 startButton.addEventListener('click', startGame);
 // if start button is pressed start timer
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex ++
+  setNextQuestion()
+});
 
   
   
@@ -21,7 +25,7 @@ function startGame() {
   console.log('game started') //logs if start button is clicked
   startButton.classList.add('hide') // hides start button once clicked and first question shows
   questionsContainerElement.classList.remove('hide') //removes hide from questions once start button is clicked
-  shuffledQuestions = questions.sort(() => Math.random() - .5) //gives random array of questions
+  shuffledQuestions = questions.sort(() => Math.random() - .5) //gives random array from questions
   currentQuestionIndex = 0
   setNextQuestion()
   
@@ -50,22 +54,42 @@ function showQuestion(question) {
 
 function resetState() {
   nextButton.classList.add('hide') // hides next button after new question appears
-  // answerButtonsElement.removeChild(answerButtonsElement.firstChild)
   while (answerButtonsElement.firstChild) {
     answerButtonsElement.removeChild(answerButtonsElement.firstChild)
   }
 
 }; //resets everything back to default state every time we get a new question
 
-function selectAnswer() {
-
+function selectAnswer(element) {
+  var selectedButton = element.target; //
+  var correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  });
+  if (shuffledQuestions.length > currentQuestionIndex +1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+  
 };
 
-// questions
-  
-  // if false subtract 15sec and move to next question
-  // store answer
-  // if timer reaches 0 stop game display score and do bellow
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+};
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+};
+
 
 const questions = [
   {
@@ -75,19 +99,25 @@ const questions = [
       { text: 'Osmosis', correct: false},
       { text: 'Rainbow Dust', correct: false},
       { text: 'Unicorn Farts', correct: false}
-    ],
+    ]
+  },
+  {
     question: 'Did you stuggle with this challenge',
     answers: [
       { text: 'Yes', correct: true},
       { text: 'A lot', correct: true},
       { text: 'Sure did',  correct: true},
       { text: 'You had better believe it', correct: true},
-    ],
-    // question: ''
-
-  },
+    ]
+  }
 
 ];
+
+// timer
+  
+  // if false subtract 15sec and move to next question
+  // store answer
+  // if timer reaches 0 stop game display score and do bellow
 
 // once all questions have been answered and score is logged
   // display score
@@ -95,7 +125,7 @@ const questions = [
   // store score  and initial in local to be called by leader board     
   // display score to user in leader board
   // display initials next to score in leader board
-  // unhide start button
+  
 
 
 
@@ -111,4 +141,5 @@ const questions = [
     // answer-buttons
     // start-button
     // question
+    // unhide start button
 
